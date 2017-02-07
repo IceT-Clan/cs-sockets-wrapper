99b7c66fd0970de48a23b4d11dcf913d7ff1a283
Chronophylos <admiral.chronophylos@web.de>
2017-02-07 20:52:38 +0100

    added docstrings

--- ClientSocket.cs
+++ ClientSocket.cs
@@ -12,6 +12,11 @@ namespace Sockets {
         private IPEndPoint endPoint;
         private Encoding encoding = Encoding.UTF8;
 
+        /// <summary>
+        /// Create a new ClientSocket object
+        /// </summary>
+        /// <param name="host">A host to connect to</param>
+        /// <param name="port">The port to connect to</param>
         public ClientSocket(string host, int port) {
             //this.socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             this.socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
@@ -21,11 +26,17 @@ namespace Sockets {
             }
             this.endPoint = new IPEndPoint (this.host,this.port);
         }
-
+        /// <summary>
+        /// Create a ClientSocket object with another ClientSocket object
+        /// </summary>
+        /// <param name="socket">a ClientSocket object</param>
         public ClientSocket(Socket socket) {
             this.socket = socket;
         }
-
+        /// <summary>
+        /// Connect to the server
+        /// </summary>
+        /// <returns>The succeess of the connection</returns>
         public bool Connect() {
             try {
                 //this.socket.Connect(this.host, this.port);
@@ -36,44 +47,64 @@ namespace Sockets {
             }
             return true;
         }
-
+        /// <summary>
+        /// Check if data is available at the socket
+        /// </summary>
+        /// <returns>Return the number of byte available</returns>
         public Int32 DataAvailable() => this.socket.Available;
-
-        public void Write(Int32 b) {
+        /// <summary>
+        /// Send a single byte to the socket
+        /// </summary>
+        /// <param name="data">The byte to send</param>
+        public void Write(Int32 data) {
             Byte[] msg = new Byte[1];
-            msg[0] = (Byte)b;
+            msg[0] = (Byte)data;
             this.socket.Send(msg);
         }
-
-        public void Write(string str) => this.socket.Send(this.encoding.GetBytes(str));
-
-        public Char Read() {
+        /// <summary>
+        /// Write a string to the socket
+        /// </summary>
+        /// <param name="data">The data to send</param>
+        public void Write(string data) => this.socket.Send(this.encoding.GetBytes(data));
+        /// <summary>
+        /// Read a single byte from the socket
+        /// </summary>
+        /// <returns>The number of bytes recieved</returns>
+        public Int32 Read() {
             Byte[] rcvbuffer = new Byte[1];
             this.socket.Receive(rcvbuffer);
             return (Char)rcvbuffer[0];
         }
-
-        public Char Read(Byte[] b, Int32 len) => (Char)this.socket.Receive(b, len, SocketFlags.None);
-
         /// <summary>
-        /// Receive bytes from server until newline
+        /// Read a specified number of bytes from the socket into a buffer
+        /// </summary>
+        /// <param name="buffer">The storeage location for the bytes</param>
+        /// <param name="lenght">The number of bytes to recieve</param>
+        /// <returns>The number of bytes recieved</returns>
+        public Int32 Read(Byte[] buffer, Int32 lenght) => this.socket.Receive(buffer, lenght, SocketFlags.None);
+        /// <summary>
+        /// Receive bytes from server until a newline is hit
         /// </summary>
         /// <returns>String with bytes from server (without newline)</returns>
         public String ReadLine() {
             String rcv = "";
-            Byte[] rcvbuffer = new Byte[1];
-            rcvbuffer[0] = 0;
+            Byte[] rcvbuffer = new Byte[1] { 0 };
 
             while (rcvbuffer[0] != '\n') {
                 this.socket.Receive(rcvbuffer);
-                ASCIIEncoding enc = new ASCIIEncoding();
+                ASCIIEncoding enc = new ASCIIEncoding(); // TODO check if this can be omitted
                 rcv += this.encoding.GetString(rcvbuffer);
             }
-            //return recv.Trim('\0');
 
             return rcv.Substring(0, rcv.Length - 1);
         }
+        /// <summary>
+        /// Close the socket
+        /// </summary>
         public void Close() => this.socket.Close();
+        /// <summary>
+        /// Dispose the socket
+        /// </summary>
         public void Dispose() => this.socket.Dispose();
     }
 }
